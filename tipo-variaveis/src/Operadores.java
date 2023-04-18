34849e7c34d904d3be311a0ad0b62665ed7b5d88
Wnd3rs0n <wandersonwos@hotmail.com>
2023-04-17 21:02:55 -0300

    Exercicio_SmartTv

---
+++ tipo-variaveis/src/Operadores.java
@@ -0,0 +1,16 @@
+public class Operadores {
+    public static void main(String[] args) {
+
+        int numero1 = 5;
+        int numero2 = 8;
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println("Numero 1 é igual a numero 2 : " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("Numero Um é diferente de Numero dois ? :" + simNao);
+
+    }
+}
