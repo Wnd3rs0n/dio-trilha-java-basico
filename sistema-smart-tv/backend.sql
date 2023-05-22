d89726d8234a9e0b5557e95cd343d31228204bd7
Wnd3rs0n <wandersonwos@hotmail.com>
2023-05-22 20:50:01 -0300

    first commitAula_dio

---
+++ sistema-smart-tv/backend.sql
@@ -0,0 +1,9 @@
+CREATE TABLE  pessoas (
+    id INT  NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR (30) NOT NULL,
+    nascimento DATE
+    )
+
+INSERT INTO pessoas (name, nascimento) VALUES ('Wanderson', '1993 05 29')
+INSERT INTO pessoas (name, nascimento) VALUES ('Jurema', '1981 03 20')
+INSERT INTO pessoas (name, nascimento) VALUES ('Jucelino', '1982 04 21')
